f5e2b76f9d61c43f18c19de04d69d0f1e74ec974
liuhuan <1589328059@qq.com>
2022-06-03 22:42:07 +0800

    修改首页页面

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,16 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import index from "./views/index.vue";
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <!-- 这是项目初始化自带的，不使用
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Hello Vue 3 + Vite" />
+  -->
+  <!-- 添加首页展示信息 -->
+  <index/>
 </template>
 
 <style>
