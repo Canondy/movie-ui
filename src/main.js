0de2c979bfa74a0b594970b434c168a2d97120c2
liuhuan <1589328059@qq.com>
2022-06-03 23:44:44 +0800

    测试Vant能否正常使用

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+// 导入Vant组件
+import Vant from 'vant'
+// 导入Vant的css样式
+import "vant/lib/index.css"
+createApp(App)
+    .use(Vant)
+    .mount('#app')
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,5 +1,6 @@
 <template>
   <p> 我是首页 ！！！</p>
+  <van-button type="success">成功按钮</van-button>
 </template>
 
 <script>
