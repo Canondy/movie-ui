c978a9a84fb931b466976ece69c071a61d94b00c
liuhuan <1589328059@qq.com>
2022-06-03 23:14:22 +0800

    添加Vant组件并配置按需引入

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import styleImport, { VantResolve } from 'vite-plugin-style-import';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
-})
+// 这个Vue3项目创建时引入的
+// export default defineConfig({
+//   plugins: [vue()]
+// })
+
+// 使用Vant组件时引入的
+export default {
+  plugins: [
+    vue(),
+    styleImport({
+      resolves: [VantResolve()],
+    }),
+  ],
+};
