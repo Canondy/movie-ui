107ca51cc2bfd00c302e012e148ed4adbd07f2c9
liuhuan <1589328059@qq.com>
2022-06-13 13:59:20 +0800

    修改登录表单校验规则

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -12,7 +12,7 @@
           name="username"
           label="用户名"
           placeholder="用户名"
-          :rules="[{ required: true, message: '请填写用户名' }]"
+          :rules="userRules.username"
       />
       <van-field
           v-model="password"
@@ -20,7 +20,7 @@
           name="password"
           label="密码"
           placeholder="密码"
-          :rules="[{ required: true, message: '请填写密码' }]"
+          :rules="userRules.password"
       />
     </van-cell-group>
     <div style="margin: 16px;">
@@ -60,6 +60,11 @@ const onSubmit = (values) => {
   }
 };
 
+// 添加表单校验规则
+const userRules = {
+  username: [{required: true, message: "请填写用户名" }],
+  password: [{required: true, message: "请填写密码" }]
+}
 </script>
 
 <style scoped>
