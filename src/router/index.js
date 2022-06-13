d44b88c7795d5d949a9585af6b50bbb03a8510ef
liuhuan <1589328059@qq.com>
2022-06-13 18:04:37 +0800

    添加底部导航栏

--- src/router/index.js
+++ src/router/index.js
@@ -4,28 +4,48 @@ import {createRouter, createWebHashHistory } from "vue-router"
 const routes = [
     {
         // 路径地址
+        path: '/',
+        // 路由规则名称
+        name: 'layout',
+        // 导入对应的组件
+        component: () => import('../views/layout/index.vue'),
+        // 子路由
+        children: [
+            {
+                // 路径地址 空串标识默认 设置的子路由
+                path: '',
+                // 路由规则名称
+                name: 'home',
+                // 导入对应的组件
+                component: () => import('../views/home/index.vue')
+            },
+            {
+                // 路径地址
+                path: '/my',
+                // 路由规则名称
+                name: 'my',
+                // 导入对应的组件
+                component: () => import('../views/my/index.vue')
+            },
+            {
+                // 路径地址
+                path: '/order',
+                // 路由规则名称
+                name: 'order',
+                // 导入对应的组件
+                component: () => import('../views/order/index.vue')
+            },
+        ]
+    },
+    {
+        // 路径地址
         path: '/login',
         // 路由规则名称
         name: 'login',
         // 导入对应的组件
         component: () => import('../views/login/index.vue')
     },
-    {
-        // 路径地址
-        path: '/my',
-        // 路由规则名称
-        name: 'my',
-        // 导入对应的组件
-        component: () => import('../views/my/index.vue')
-    },
-    {
-        // 路径地址
-        path: '/order',
-        // 路由规则名称
-        name: 'order',
-        // 导入对应的组件
-        component: () => import('../views/order/index.vue')
-    },
+
 ];
 // 2.创建路由实例
 const router = createRouter({
---
+++ src/views/layout/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <!-- 子路由的出口   -->
+    <router-view></router-view>
+    <!-- 构建标签导航栏   -->
+    <van-tabbar v-model="active">
+      <van-tabbar-item icon="home-o" to="/">首页</van-tabbar-item>
+      <van-tabbar-item icon="cart-o" to="/order">订单</van-tabbar-item>
+      <van-tabbar-item icon="contact" to="/my">我的</van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+// ref(0) 默认选中第一个
+const active = ref(0);
+
+</script>
+
+<style scoped>
+
+</style>
